bb4f15e5ed92852d3b7d19fcab5c78466ecfa7d2
simenbkr <simen@haitech.org>
2017-04-09 18:38:17 +0200

    Initiate work on mainloop for the controller. Add needed features to DAOs for ease of use with regards to mainloop.

--- src/main/java/data/dao/SensorHistoryDAO.java
+++ src/main/java/data/dao/SensorHistoryDAO.java
@@ -1,4 +1,5 @@
 package data.dao;
+
 import data.db.DB;
 import data.mapper.PlantMapper;
 import data.mapper.SensorHistoryMapper;
@@ -15,29 +16,28 @@ import java.util.List;
 public class SensorHistoryDAO implements IDAO<SensorHistory> {
     @Override
     public void update(SensorHistory sensorHistory) {
-    	String sql = "UPDATE termin8_django_sensorHistory SET temp=?,moisture=?,timestamp=?,plant_id=? WHERE id=?";
-    	
+        String sql = "UPDATE termin8_django_sensorHistory SET temp=?,moisture=?,timestamp=?,plant_id=? WHERE id=?";
+
         try {
             Connection connection = DB.getConnection();
             PreparedStatement st = connection.prepareStatement(sql);
             st.setDouble(1, sensorHistory.getTemp());
-            st.setDouble(2,sensorHistory.getMoisture());
+            st.setDouble(2, sensorHistory.getMoisture());
             st.setTimestamp(3, sensorHistory.getTime());
             st.setInt(4, sensorHistory.getPlant_id());
             st.setInt(5, sensorHistory.getId());
-        
+
             st.executeUpdate();
-            connection.close(); 
-        } catch(SQLException e){
+            connection.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    
 
     @Override
     public void delete(SensorHistory sensorHistory) {
-    	String sql = "DELETE FROM termin8_django_sensorHistory WHERE id=?";
+        String sql = "DELETE FROM termin8_django_sensorHistory WHERE id=?";
         try {
             Connection connection = DB.getConnection();
             PreparedStatement st = connection.prepareStatement(sql);
@@ -52,7 +52,7 @@ public class SensorHistoryDAO implements IDAO<SensorHistory> {
 
     @Override
     public int create(SensorHistory sensorHistory) {
-    	String sql = "INSERT INTO termin8_django_sensorHistory (temp,moisture,timestamp,plant_id) VALUES(?,?,?,?)";
+        String sql = "INSERT INTO termin8_django_sensorHistory (temp,moisture,timestamp,plant_id) VALUES(?,?,?,?)";
         int lastId = -1;
         try {
             Connection connection = DB.getConnection();
@@ -61,14 +61,14 @@ public class SensorHistoryDAO implements IDAO<SensorHistory> {
             st.setDouble(2, sensorHistory.getMoisture());
             st.setTimestamp(3, sensorHistory.getTime());
             st.setInt(4, sensorHistory.getPlant_id());
-            
+
             st.execute();
             ResultSet rs = st.getGeneratedKeys();
-            if(rs.next()){
+            if (rs.next()) {
                 lastId = rs.getInt(1);
             }
             connection.close();
-        } catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return lastId;
@@ -76,19 +76,19 @@ public class SensorHistoryDAO implements IDAO<SensorHistory> {
 
     @Override
     public List<SensorHistory> listAll() {
-    	List<SensorHistory> sensorHistoryList = new ArrayList<>();
+        List<SensorHistory> sensorHistoryList = new ArrayList<>();
         String sql = "SELECT * FROM termin8_django_sensorHistory";
         try {
             Connection connection = DB.getConnection();
             ResultSet rs = connection.createStatement().executeQuery(sql);
             rs.beforeFirst();
             SensorHistoryMapper mapper = new SensorHistoryMapper();
-            while(rs.next()){
-            	sensorHistoryList.add(mapper.mapRow(rs, rs.getRow()));
+            while (rs.next()) {
+                sensorHistoryList.add(mapper.mapRow(rs, rs.getRow()));
             }
             connection.close();
             return sensorHistoryList;
-        } catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
@@ -96,17 +96,35 @@ public class SensorHistoryDAO implements IDAO<SensorHistory> {
 
     @Override
     public SensorHistory getByID(int id) {
-    	String sql = "SELECT * FROM termin8_django_sensorHistory WHERE id=" + String.valueOf(id);
+        String sql = "SELECT * FROM termin8_django_sensorHistory WHERE id=" + String.valueOf(id);
         try {
             Connection connection = DB.getConnection();
             ResultSet rs = connection.createStatement().executeQuery(sql);
             rs.beforeFirst();
             rs.next();
             return new SensorHistoryMapper().mapRow(rs, 0);
-        } catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
     }
-   }
+
+    public SensorHistory getLastByPlantId(int plant_id) {
+        String sql = "SELECT * FROM termin8_django_sensorhistory WHERE plant_id=? ORDER BY id DESC LIMIT 1";
+        try {
+            Connection connection = DB.getConnection();
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, plant_id);
+            ResultSet rs = st.executeQuery();
+            rs.beforeFirst();
+            rs.next();
+            connection.close();
+            return new SensorHistoryMapper().mapRow(rs, 0);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
 
