dd649343dca83c039d4674a2b5a82fea8095a1dd
simenbkr <simen@haitech.org>
2017-04-11 16:11:04 +0200

    Bugfix/update in table for user and plant relation (to reflect changes done with the model (in django) ).

--- src/main/java/data/dao/UserPlantDAO.java
+++ src/main/java/data/dao/UserPlantDAO.java
@@ -16,25 +16,25 @@ import java.util.List;
 public class UserPlantDAO implements IDAO<UserPlant> {
     @Override
     public void update(UserPlant userPlant) {
-String sql = "UPDATE termin8_django_userownsplant SET plant_id=?, user_id=? WHERE id=?";
-    	
+        String sql = "UPDATE termin8_django_plant_owned_by SET plant_id=?, user_id=? WHERE id=?";
+
         try {
             Connection connection = DB.getConnection();
             PreparedStatement st = connection.prepareStatement(sql);
             st.setInt(1, userPlant.getPlant_id());
             st.setInt(2, userPlant.getUser_id());
             st.setInt(3, userPlant.getId());
-        
+
             st.executeUpdate();
-            connection.close(); 
-        } catch(SQLException e){
+            connection.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     @Override
     public void delete(UserPlant userPlant) {
-    	String sql = "DELETE FROM termin8_django_userownsplant WHERE id=?";
+        String sql = "DELETE FROM termin8_django_plant_owned_by WHERE id=?";
         try {
             Connection connection = DB.getConnection();
             PreparedStatement st = connection.prepareStatement(sql);
@@ -49,21 +49,21 @@ String sql = "UPDATE termin8_django_userownsplant SET plant_id=?, user_id=? WHER
 
     @Override
     public int create(UserPlant userPlant) {
-    	String sql = "INSERT INTO termin8_django_userownsplant (plant_id,user_id) VALUES(?,?)";
+        String sql = "INSERT INTO termin8_django_plant_owned_by (plant_id,user_id) VALUES(?,?)";
         int lastId = -1;
         try {
             Connection connection = DB.getConnection();
             PreparedStatement st = connection.prepareStatement(sql);
             st.setInt(1, userPlant.getPlant_id());
             st.setInt(2, userPlant.getUser_id());
-            
+
             st.execute();
             ResultSet rs = st.getGeneratedKeys();
-            if(rs.next()){
+            if (rs.next()) {
                 lastId = rs.getInt(1);
             }
             connection.close();
-        } catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return lastId;
@@ -72,19 +72,19 @@ String sql = "UPDATE termin8_django_userownsplant SET plant_id=?, user_id=? WHER
 
     @Override
     public List<UserPlant> listAll() {
-    	List<UserPlant> userPlantList = new ArrayList<>();
-        String sql = "SELECT * FROM termin8_django_sensorHistory";
+        List<UserPlant> userPlantList = new ArrayList<>();
+        String sql = "SELECT * FROM termin8_django_plant_owned_by";
         try {
             Connection connection = DB.getConnection();
             ResultSet rs = connection.createStatement().executeQuery(sql);
             rs.beforeFirst();
             UserPlantMapper mapper = new UserPlantMapper();
-            while(rs.next()){
-            	userPlantList.add(mapper.mapRow(rs, rs.getRow()));
+            while (rs.next()) {
+                userPlantList.add(mapper.mapRow(rs, rs.getRow()));
             }
             connection.close();
             return userPlantList;
-        } catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
@@ -92,16 +92,16 @@ String sql = "UPDATE termin8_django_userownsplant SET plant_id=?, user_id=? WHER
 
     @Override
     public UserPlant getByID(int id) {
-    	String sql = "SELECT * FROM termin8_django_sensorHistory WHERE id=" + String.valueOf(id);
+        String sql = "SELECT * FROM termin8_django_plant_owned_by WHERE id=" + String.valueOf(id);
         try {
             Connection connection = DB.getConnection();
             ResultSet rs = connection.createStatement().executeQuery(sql);
             rs.beforeFirst();
             rs.next();
             return new UserPlantMapper().mapRow(rs, 0);
-        } catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
     }
-   }
+}
